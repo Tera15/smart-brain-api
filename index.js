aa48a62b249b2b79886345d819598cab24179b1b
Tera15 <ccatherall15@gmail.com>
2020-02-12 21:28:51 -0800

    setting up deploy

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ const knex = require('knex');
    const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'ccatherall',
-      password : '',
-      database : 'smart-brain'
+      connectString : process.env.DATABASE_URL,
+      ssl: true,
+    
     }
   });
 

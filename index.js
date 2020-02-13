ef750a2a8b49405ee7cb9eab434a4c466af923e2
Tera15 <ccatherall15@gmail.com>
2020-02-12 21:41:45 -0800

    changed route

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(cors())
 
 app.get('/', (req,res) => {
-    res.send(database.users ); 
+    res.send('hi its working'); 
 })
 
 app.post('/signin', (req, res)=>{
